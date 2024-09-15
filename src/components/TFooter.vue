1a93cf5fa144080be4ff8491f74abb0a487da0b5
Your Name <styvesdaudet@gmail.com>
2024-09-15 08:40:13 +0100

    footer

--- src/components/TFooter.vue
+++ src/components/TFooter.vue
@@ -1,88 +1,329 @@
 <template>
-    <section  class="bg-black">
-    <footer class="footer text-white justify-between p-10  ">
-        <nav class="lg:px-10">
-          <h6 class="footer-title">Services</h6>
-          <a class="link link-hover">Achetez un livre</a>
-          <a class="link link-hover">Livres gratuits</a>
-          <a class="link link-hover">Téléchargement</a>
-          <a class="link link-hover">Lecture</a>
-        </nav>
-        <nav>
-          <h6 class="footer-title">Compagnie</h6>
-          <a class="link link-hover">Laissez nous un message</a>
-          <a class="link link-hover">à propos de nous</a>
-          <a class="link link-hover">Contact</a>
-        </nav>
-        <nav class="lg:px-10">
-          <h6 class="footer-title">Politique</h6>
-          <a class="link link-hover">Politique de confidentialité</a>
-          <a class="link link-hover">termes d'utilisation</a>
-          <a class="link link-hover">politique de cookies</a>
-        </nav>
-      </footer>
-
-
-      <footer class="footer  bg-black text-white border-base-300 border-t px-10 py-4">
-        <aside class="grid-flow-col items-center lg:px-8">
-          <img 
-          src="../assets/images/logo.png"
-          alt=""
-          class="h-12 w-12 rounded-full lg:h-16 lg:w-16 "
-        />
-          <i>
-            M_LIBRARY Ltd.
-            <br />
-            Avec vous depuis 1992
-          </i>
-        </aside>
-
-
-        <nav class="md:place-self-center md:justify-self-end lg:px-10">
-          <div class="grid grid-flow-col gap-4">
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                class="fill-current">
-                <path
-                  d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-              </svg>
-            </a>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                class="fill-current">
-                <path
-                  d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-              </svg>
-            </a>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                class="fill-current">
-                <path
-                  d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-              </svg>
+  <section class="bg-[#24303F]">
+    <footer class="footer-container">
+      <div class="mx-auto  px-4 sm:px-6 lg:px-8 container">
+        <!--Grid-->
+        <div class=" flex max-sm:max-w-sm max-sm:mx-auto">
+          <div class="col-span-full mb-10 lg:col-span-2 lg:mb-0 border-r border-gray-700">
+            <img src="../assets/images/logo18.png" alt="" height="0" width="225" />
+            <p class="py-8 text-sm text-gray-500 lg:max-w-xs text-center lg:text-left">Trusted in more than 100
+              countries & 5 million customers. Have any query ?</p>
+            <a href="javascript:;"
+              class="py-2.5 px-5 h-9 block w-fit bg-[#E01520] rounded-full shadow-sm text-xs text-white mx-auto transition-all  duration-500 hover:bg-red-500 lg:mx-0">
+              Contact us
             </a>
           </div>
-        </nav>
-      </footer>
-      </section>
+          <!--End Col-->
+          <div class="text-center border-r border-gray-700 px-20">
+            <h4 class="text-lg text-white font-bold mb-7 uppercase font-heading">Votre compte</h4>
+            <ul class="text-sm  transition-all duration-500">
+              <li class="mb-6"><a href="javascript:;" class="text-gray-600 hover:text-gray-900">Home</a></li>
+              <li class="mb-6"><a href="javascript:;" class=" text-gray-600 hover:text-gray-900">About</a></li>
+              <li class="mb-6"><a href="javascript:;" class=" text-gray-600 hover:text-gray-900">Pricing</a></li>
+            </ul>
+          </div>
+          <!--End Col-->
+          <div class="text-center border-r border-gray-700 px-16">
+            <h4 class="text-lg text-white font-bold mb-7 uppercase font-heading">Informations</h4>
+            <ul class="text-sm  transition-all duration-500">
+              <li class="mb-6"><a href="javascript:;" class="text-gray-600 hover:text-gray-900">Figma UI System</a>
+              </li>
+              <li class="mb-6"><a href="javascript:;" class=" text-gray-600 hover:text-gray-900">Icons Assets</a>
+              </li>
+              <li class="mb-6"><a href="javascript:;" class=" text-gray-600 hover:text-gray-900">Responsive
+                  Blocks</a></li>
+            </ul>
+          </div>
+          <!--End Col-->
+          <div class="text-center px-16 block_newsletter ">
+            <h4 class="text-lg text-white font-bold mb-7 uppercase font-heading">Lettre d'informations
+            </h4>
+            <p class="lg:max-w-xs text-sm  transition-all duration-500 text-gray-600">Vous pouvez
+              vous désinscrire à tout moment. Vous
+              trouverez pour cela nos informations
+              de contact dans les
+              conditions d'utilisation du site.</p>
+            <form class="mt-4">
+              <div class="block-newsletter-input">
+                <input type="email" name="email" value="" placeholder="Votre e-mail" required="">
+                <input class="btn btn-primary" type="submit" value="Subscribe" name="submitNewsletter">
+              </div>
+            </form>
+
+          </div>
+        </div>
+        <!--Grid-->
+      </div>
+    </footer>
+    <footer>
+      <div class="py-7 border-t border-gray-200 ">
+        <div class="container flex items-center justify-center flex-col lg:justify-between lg:flex-row">
+          <span class="text-sm text-gray-500 ">©<a href="https://pagedone.io/">pagedone</a> 2024, All rights
+            reserved.</span>
+          <div class="flex mt-4 space-x-4 sm:justify-center lg:mt-0 ">
+            <a href="javascript:;"
+              class="w-9 h-9 rounded-full bg-gray-700 flex justify-center items-center hover:bg-indigo-600">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                <g id="Social Media">
+                  <path id="Vector"
+                    d="M11.3214 8.93666L16.4919 3.05566H15.2667L10.7772 8.16205L7.1914 3.05566H3.05566L8.47803 10.7774L3.05566 16.9446H4.28097L9.022 11.552L12.8088 16.9446H16.9446L11.3211 8.93666H11.3214ZM9.64322 10.8455L9.09382 10.0765L4.72246 3.95821H6.60445L10.1322 8.8959L10.6816 9.66481L15.2672 16.083H13.3852L9.64322 10.8458V10.8455Z"
+                    fill="white" />
+                </g>
+              </svg>
+            </a>
+            <a href="javascript:;"
+              class="w-9 h-9 rounded-full bg-gray-700 flex justify-center items-center hover:bg-indigo-600">
+              <svg class="w-[1.25rem] h-[1.125rem] text-white" viewBox="0 0 15 15" fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M4.70975 7.93663C4.70975 6.65824 5.76102 5.62163 7.0582 5.62163C8.35537 5.62163 9.40721 6.65824 9.40721 7.93663C9.40721 9.21502 8.35537 10.2516 7.0582 10.2516C5.76102 10.2516 4.70975 9.21502 4.70975 7.93663ZM3.43991 7.93663C3.43991 9.90608 5.05982 11.5025 7.0582 11.5025C9.05658 11.5025 10.6765 9.90608 10.6765 7.93663C10.6765 5.96719 9.05658 4.37074 7.0582 4.37074C5.05982 4.37074 3.43991 5.96719 3.43991 7.93663ZM9.97414 4.22935C9.97408 4.39417 10.0236 4.55531 10.1165 4.69239C10.2093 4.82946 10.3413 4.93633 10.4958 4.99946C10.6503 5.06259 10.8203 5.07916 10.9844 5.04707C11.1484 5.01498 11.2991 4.93568 11.4174 4.81918C11.5357 4.70268 11.6163 4.55423 11.649 4.39259C11.6817 4.23095 11.665 4.06339 11.6011 3.91109C11.5371 3.7588 11.4288 3.6286 11.2898 3.53698C11.1508 3.44536 10.9873 3.39642 10.8201 3.39635H10.8197C10.5955 3.39646 10.3806 3.48424 10.222 3.64043C10.0635 3.79661 9.97434 4.00843 9.97414 4.22935ZM4.21142 13.5892C3.52442 13.5584 3.15101 13.4456 2.90286 13.3504C2.57387 13.2241 2.33914 13.0738 2.09235 12.8309C1.84555 12.588 1.69278 12.3569 1.56527 12.0327C1.46854 11.7882 1.3541 11.4201 1.32287 10.7431C1.28871 10.0111 1.28189 9.79119 1.28189 7.93669C1.28189 6.08219 1.28927 5.86291 1.32287 5.1303C1.35416 4.45324 1.46944 4.08585 1.56527 3.84069C1.69335 3.51647 1.84589 3.28513 2.09235 3.04191C2.3388 2.79869 2.57331 2.64813 2.90286 2.52247C3.1509 2.42713 3.52442 2.31435 4.21142 2.28358C4.95417 2.24991 5.17729 2.24319 7.0582 2.24319C8.9391 2.24319 9.16244 2.25047 9.90582 2.28358C10.5928 2.31441 10.9656 2.42802 11.2144 2.52247C11.5434 2.64813 11.7781 2.79902 12.0249 3.04191C12.2717 3.2848 12.4239 3.51647 12.552 3.84069C12.6487 4.08513 12.7631 4.45324 12.7944 5.1303C12.8285 5.86291 12.8354 6.08219 12.8354 7.93669C12.8354 9.79119 12.8285 10.0105 12.7944 10.7431C12.7631 11.4201 12.6481 11.7881 12.552 12.0327C12.4239 12.3569 12.2714 12.5882 12.0249 12.8309C11.7784 13.0736 11.5434 13.2241 11.2144 13.3504C10.9663 13.4457 10.5928 13.5585 9.90582 13.5892C9.16306 13.6229 8.93994 13.6296 7.0582 13.6296C5.17645 13.6296 4.95395 13.6229 4.21142 13.5892ZM4.15307 1.03424C3.40294 1.06791 2.89035 1.18513 2.4427 1.3568C1.9791 1.53408 1.58663 1.77191 1.19446 2.1578C0.802277 2.54369 0.56157 2.93108 0.381687 3.38797C0.207498 3.82941 0.0885535 4.3343 0.0543922 5.07358C0.0196672 5.81402 0.0117188 6.05074 0.0117188 7.93663C0.0117188 9.82252 0.0196672 10.0592 0.0543922 10.7997C0.0885535 11.539 0.207498 12.0439 0.381687 12.4853C0.56157 12.9419 0.802334 13.3297 1.19446 13.7155C1.58658 14.1012 1.9791 14.3387 2.4427 14.5165C2.89119 14.6881 3.40294 14.8054 4.15307 14.839C4.90479 14.8727 5.1446 14.8811 7.0582 14.8811C8.9718 14.8811 9.212 14.8732 9.96332 14.839C10.7135 14.8054 11.2258 14.6881 11.6737 14.5165C12.137 14.3387 12.5298 14.1014 12.9219 13.7155C13.3141 13.3296 13.5543 12.9419 13.7347 12.4853C13.9089 12.0439 14.0284 11.539 14.062 10.7997C14.0962 10.0587 14.1041 9.82252 14.1041 7.93663C14.1041 6.05074 14.0962 5.81402 14.062 5.07358C14.0278 4.33424 13.9089 3.82913 13.7347 3.38797C13.5543 2.93135 13.3135 2.5443 12.9219 2.1578C12.5304 1.7713 12.137 1.53408 11.6743 1.3568C11.2258 1.18513 10.7135 1.06735 9.96388 1.03424C9.21256 1.00058 8.97236 0.992188 7.05876 0.992188C5.14516 0.992188 4.90479 1.00002 4.15307 1.03424Z"
+                  fill="currentColor" />
+              </svg>
+
+            </a>
+            <a href="javascript:;"
+              class="w-9 h-9 rounded-full bg-gray-700 flex justify-center items-center hover:bg-indigo-600">
+              <svg class="w-[1rem] h-[1rem] text-white" viewBox="0 0 13 12" fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M2.8794 11.5527V3.86835H0.318893V11.5527H2.87967H2.8794ZM1.59968 2.81936C2.4924 2.81936 3.04817 2.2293 3.04817 1.49188C3.03146 0.737661 2.4924 0.164062 1.61666 0.164062C0.74032 0.164062 0.167969 0.737661 0.167969 1.49181C0.167969 2.22923 0.723543 2.8193 1.5829 2.8193H1.59948L1.59968 2.81936ZM4.29668 11.5527H6.85698V7.26187C6.85698 7.03251 6.87369 6.80255 6.94134 6.63873C7.12635 6.17968 7.54764 5.70449 8.25514 5.70449C9.18141 5.70449 9.55217 6.4091 9.55217 7.44222V11.5527H12.1124V7.14672C12.1124 4.78652 10.8494 3.68819 9.16483 3.68819C7.78372 3.68819 7.17715 4.45822 6.84014 4.98267H6.85718V3.86862H4.29681C4.33023 4.5895 4.29661 11.553 4.29661 11.553L4.29668 11.5527Z"
+                  fill="currentColor" />
+              </svg>
+
+            </a>
+            <a href="javascript:;"
+              class="w-9 h-9 rounded-full bg-gray-700 flex justify-center items-center hover:bg-indigo-600">
+              <svg class="w-[1.25rem] h-[0.875rem] text-white" viewBox="0 0 16 12" fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd"
+                  d="M13.9346 1.13529C14.5684 1.30645 15.0665 1.80588 15.2349 2.43896C15.5413 3.58788 15.5413 5.98654 15.5413 5.98654C15.5413 5.98654 15.5413 8.3852 15.2349 9.53412C15.0642 10.1695 14.5661 10.669 13.9346 10.8378C12.7886 11.1449 8.19058 11.1449 8.19058 11.1449C8.19058 11.1449 3.59491 11.1449 2.44657 10.8378C1.81277 10.6666 1.31461 10.1672 1.14622 9.53412C0.839844 8.3852 0.839844 5.98654 0.839844 5.98654C0.839844 5.98654 0.839844 3.58788 1.14622 2.43896C1.31695 1.80353 1.81511 1.30411 2.44657 1.13529C3.59491 0.828125 8.19058 0.828125 8.19058 0.828125C8.19058 0.828125 12.7886 0.828125 13.9346 1.13529ZM10.541 5.98654L6.72178 8.19762V3.77545L10.541 5.98654Z"
+                  fill="currentColor" />
+              </svg>
+
+            </a>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+  </section>
 </template>
 
 <script setup>
 
 </script>
 
-<style  scoped>
+<style scoped>
+.footer-container {
+  overflow: hidden;
+  padding: 70px 0 60px 0
+}
 
+.container {
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 15px;
+  padding-right: 15px
+}
+
+.container:after {
+  content: "";
+  display: table;
+  clear: both
+}
+
+@media (min-width: 576px) {
+  .container {
+    width: 540px;
+    max-width: 100%
+  }
+}
+
+@media (min-width: 768px) {
+  .container {
+    width: 720px;
+    max-width: 100%
+  }
+}
+
+@media (min-width: 992px) {
+  .container {
+    width: 960px;
+    max-width: 100%
+  }
+}
+
+@media (min-width: 1200px) {
+  .container {
+    width: 1400px;
+    max-width: 100%
+  }
+}
+
+@media (min-width: 1350px) and (max-width:1450px) {
+  .container {
+    width: 1300px;
+    max-width: 100%
+  }
+}
+
+@media (min-width: 1200px) and (max-width:1349px) {
+  .container {
+    width: 1170px;
+    max-width: 100%
+  }
+}
+
+.container-fluid {
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 15px;
+  padding-right: 15px
+}
+
+.container-fluid:after {
+  content: "";
+  display: table;
+  clear: both
+}
+
+.block_newsletter {
+  width: 25%;
+  float: left;
+  text-align: center;
+  padding-right: 0;
+  padding-left: 30px
+}
+
+.block_newsletter form {
+  position: relative
+}
+
+.block_newsletter form input[type=text] {
+  border: none;
+  padding: 10px;
+  min-width: 255px;
+  color: #7a7a7a;
+  border: 1px solid rgba(0, 0, 0, .25)
+}
+
+.block_newsletter form input[type=text]:focus {
+  outline: 3px solid #ffb85f;
+  color: #232323;
+  background: #fff
+}
+
+.block_newsletter form input[type=text]:focus+button .search {
+  color: #ffb85f
+}
+
+.block_newsletter form button[type=submit] {
+  position: absolute;
+  background: none;
+  border: none;
+  bottom: .3125rem;
+  right: .125rem;
+  color: #7a7a7a
+}
+
+.block_newsletter form button[type=submit] .search:hover {
+  color: #ffb85f
+}
+
+#block-newsletter-desc {
+  color: #dfdfdf
+}
+
+.block_newsletter p {
+  color: rgb(75 85 99, 0.6);
+  line-height: 23px;
+  font-size: 13px;
+  font-weight: 400
+}
+
+.block_newsletter .alert {
+  color: #000
+}
+
+.block_newsletter #block-newsletter-label {
+  font-weight: 700;
+  text-transform: uppercase;
+  position: relative
+}
+
+.block_newsletter .block-newsletter-detail {
+  width: 100%
+}
+
+.block-newsletter-subdetail {
+  padding: 0
+}
+
+.block-newsletter-detail p:first-child {
+  display: none
+}
+
+.block-newsletter-left .col-xs-12 {
+  padding: 25px 0 10px 0
+}
+
+.block-newsletter-left {
+  width: 100%;
+  padding: 0
+}
+
+.block_newsletter .btn.btn-primary {
+  height: 30px;
+  font-size: 0;
+  width: 30px;
+  padding: 0;
+  background: #E01520 url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA4AAABOCAYAAAAQECe9AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAIGNIUk0AAHolAACAgwAA+f8AAIDpAAB1MAAA6mAAADqYAAAXb5JfxUYAAAEcSURBVHja7Ne/K0dRGMfx56IMSiYDicgmUgbK8p38DUZlQv4Ci1FWI4XVooTBwO4PkFkpxKBk0svgXq7r9g0j51OnzvPj/ZzT6TnDkyF+qNmIaAS+uxo49Kab7wCTOMOLD602A8awkydeYQ8PuT1TBwxio1R9F3O4zu17dJaBbqzhqQQtYBR3Jd8BItCLFdyUgs+YRh9ufdZiAe5XAhcYRlflpEIjBTiEZZxiHW3owWMNdImsAOvWknptFTktla6YiojtiLiKiPmIOKrET953lZP6S9Vbc984NvNrDhS5ddc8zh+lo+JvLxWLrKbJJyIii4jzZp2e/eJ3RER8eZwEJjCBCUxgAhOYwAQm8E+Abb9g0vyY5sf/MT++DgDgUDJ1YtqYBwAAAABJRU5ErkJggg==) no-repeat center 8px !important;
+  margin: 4px;
+  position: absolute;
+  right: 0;
+  top: 0
+}
+
+.block-newsletter-detail p {
+  padding-top: 25px;
+  letter-spacing: .72px;
+  text-align: left
+}
+
+.block-newsletter-detail p.alert {
+  background: #f4f4f4;
+  color: #000;
+  padding: 10px;
+  margin-top: 10px;
+  font-weight: 700
+}
+
+.block_newsletter .btn.btn-primary:hover {
+  background-position: center -56px
+}
+
+.block_newsletter form .input-wrapper {
+  overflow: hidden
+}
+
+.block_newsletter form input[type=email] {
+  padding: 0 20px;
+  display: block;
+  background: #363a42;
+  color: #dfdfdf
+}
+
+.block-newsletter-input {
+  border: 2px solid #4e5259;
+  padding: 0;
+  position: relative;
+  background-color: transparent;
+}
+
+.block_newsletter form input {
+  height: 40px;
+  border: transparent;
+  background-color: transparent !important;
+  outline: none;
+}
 </style>
