61693e5543d5848a16b22e3dd7a514f68ecb59eb
Your Name <styvesdaudet@gmail.com>
2025-03-12 11:47:01 +0100

    catalogue auto

--- src/components/LatestArticlesComponent.vue
+++ src/components/LatestArticlesComponent.vue
@@ -1,119 +1,25 @@
 <script setup lang="ts">
 import comics from "@/assets/categories/comics.jpg";
-import harlequin from "@/assets/categories/harlequin.jpg"
 import hot_romance from "@/assets/categories/hot_romance.jpeg"
 import romance_thriller from "@/assets/categories/romance_thriller.jpg"
 import marvel from "@/assets/categories/marvel.webp"
 import manga from "@/assets/categories/manga.jpg"
-import SingleScroll from "./SingleScroll.vue";
+import { getDocumentsWithFilerGlobal } from "../lib/appwrite";
+import { catalogueCollection } from "../utilities/constants";
+import { Query } from "appwrite";
+import { ref } from "vue";
 
-
-const articles = [
-  {
-    id: 1,
-    title: 'LITTERATURE',
-    content:
-      "c1.content",
-    cover: manga,
-  },
-  {
-    id: 2,
-    title: 'HARLEQUINS',
-    content:
-      "c2.content",
-    cover: hot_romance,
-  },
-  {
-    id: 3,
-    title: 'POLICE THRILLER',
-    content: "c3.content",
-    cover: romance_thriller,
-  },
-  {
-    id: 4,
-    title: 'NOBEL PRIZE',
-    content: "c4.content",
-    cover: marvel,
-  },
-  {
-    id: 5,
-    title: 'ADOS 15-21',
-    content:
-      "c5.content",
-    cover: comics,
-  },
-
-  {
-    id: 6,
-    title: 'ENFANT KID 8-14',
-    content:
-      "c6.content",
-    cover: manga,
-  },
-  {
-    id: 7,
-    title: 'ENFANTS KIDS 1-7',
-    content:
-      "c7.content",
-    cover: manga,
-  },
-  {
-    id: 8,
-    title: 'MEDECINE',
-    content:
-      "c8.content",
-    cover: manga,
-  },
-  {
-    id: 9,
-    title: 'TECHNOLOGY',
-    content:
-      "c9.content",
-    cover: manga,
-  },
-  {
-    id: 10,
-    title: 'COMICS MARVEL',
-    content:
-      "c10.content", cover: manga,
-  },
-  {
-    id: 11,
-    title: 'AFRICA AUTHORS',
-    content:
-      "c11.content", cover: manga,
-  },
-  {
-    id: 12,
-    title: 'MANGAS BD',
-    content:
-      "c12.content", cover: manga,
-  },
-  /*{
-   id: 4,
-   title: 'jeunesse/youth academic',
-   author: 'Claire Robinson',
-   content:
-     "The world is getting smaller and we're becoming more mobile. So why should you be forced to only receive money in a single ...",
-   cover: manga,
- },
- {
-   id: 4,
-   title: 'enfants/kids 1-7 ans/years',
-   author: 'Claire Robinson',
-   content:
-     "The world is getting smaller and we're becoming more mobile. So why should you be forced to only receive money in a single ...",
-   cover: manga,
- },
- {
-   id: 4,
-   title: '7-14 ans/years',
-   author: 'Claire Robinson',
-   content:
-     "The world is getting smaller and we're becoming more mobile. So why should you be forced to only receive money in a single ...",
-   cover: manga,
- },*/
-]
+const articles = ref();
+const fetchCatague = async () => {
+  const result = await getDocumentsWithFilerGlobal(catalogueCollection, [Query.equal('status', "active")]);
+  if (result.documents != null && result.documents.length > 0) {
+    articles.value = result.documents;
+  } else {
+    articles.value = [];
+  }
+  console.log(result);
+}
+fetchCatague();
 </script>
 
 <template>
@@ -126,15 +32,16 @@ const articles = [
       <div class="md:grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 hidden">
         <article v-for="article in articles" :key="article.id"
           class="w-full h-auto rounded-md bg-brand-white overflow-hidden">
-          <img :src="article.cover" class="w-full h-[100px] md:h-[200px] object-cover" alt="cover" />
+          <img :src="article.image" class="w-full h-[100px] md:h-[200px] object-cover" alt="cover" />
           <div class="p-2 md:p-6">
             <h4
               class="text-sm text-body text-brand-dark-blue leading-snug font-heading mb-2 transition-all duration-200 ease hover:text-red-600 uppercase">
-              <a href="https://www.libib.com/u/wandabook ">
+              <a :href="article.url">
                 {{ article.title }}
               </a>
-            </h4>
-            <p class="text-brand-grayish-blue text-sm hidden md:block">{{ $t(article.content) }}</p>
+            </h4> <template v-if="article.description">
+              <p class="text-brand-grayish-blue text-sm hidden md:block">{{ $t(article.description) }}</p>
+            </template>
           </div>
         </article>
       </div>
@@ -148,15 +55,17 @@ const articles = [
       <div class="grid grid-cols-3  gap-1 px-4 ">
         <article v-for="article in articles" :key="article.id"
           class="w-full h-auto rounded-md bg-brand-white overflow-hidden">
-          <img :src="article.cover" class="w-full h-[100px] md:h-[200px] object-cover" alt="cover" />
+          <img :src="article.image" class="w-full h-[100px] md:h-[200px] object-cover" alt="cover" />
           <div class="p-2 md:p-6">
             <h4
               class="text-sm text-body text-brand-dark-blue leading-snug font-heading mb-2 transition-all duration-200 ease hover:text-red-600 uppercase">
-              <a href="https://www.libib.com/u/wandabook ">
+              <a :href="article.url">
                 {{ article.title }}
               </a>
             </h4>
-            <p class="text-brand-grayish-blue text-sm hidden md:block">{{ $t(article.content) }}</p>
+            <template v-if="article.description">
+              <p class="text-brand-grayish-blue text-sm hidden md:block">{{ $t(article.description) }}</p>
+            </template>
           </div>
         </article>
       </div>
