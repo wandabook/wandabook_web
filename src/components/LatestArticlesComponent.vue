51227e2b0811e98e6de155d42944cc2be320d866
Your Name <styvesdaudet@gmail.com>
2025-03-14 18:46:50 +0100

    doc

--- src/components/LatestArticlesComponent.vue
+++ src/components/LatestArticlesComponent.vue
@@ -4,6 +4,18 @@ import hot_romance from "@/assets/categories/hot_romance.jpeg"
 import romance_thriller from "@/assets/categories/romance_thriller.jpg"
 import marvel from "@/assets/categories/marvel.webp"
 import manga from "@/assets/categories/manga.jpg"
+import ado_15_21 from "@/assets/images/categories/ado_15_21.jpg"
+import african from "@/assets/images/categories/african.jpg"
+import Comics from "@/assets/images/categories/Comics.jpg"
+import enfant_1_7 from "@/assets/images/categories/enfant_1_7.jpg"
+import harlequins from "@/assets/images/categories/harlequins.jpg"
+import kid_8_14 from "@/assets/images/categories/kid_8_14.jpg"
+import literrature from "@/assets/images/categories/literrature.png"
+import mangas from "@/assets/images/categories/mangas.jpg"
+import medecine from "@/assets/images/categories/medecine.jpg"
+import nobel_price from "@/assets/images/categories/nobel_price.jpg"
+import police_thriller from "@/assets/images/categories/police_thriller.jpg"
+import tchnologie from "@/assets/images/categories/tchnologie.jpg"
 import { getDocumentsWithFilerGlobal } from "../lib/appwrite";
 import { catalogueCollection } from "../utilities/constants";
 import { Query } from "appwrite";
