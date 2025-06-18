5091ea2bbb22168da987ed32ad02769db69b0b33
Your Name <styvesdaudet@gmail.com>
2025-06-18 20:15:13 +0100

    bolf

--- src/components/CtaComponent.vue
+++ src/components/CtaComponent.vue
@@ -6,11 +6,11 @@ const gotoWandaBook = () => {
 
 <template>
   <button @click="gotoWandaBook"
-    class=" md:block hidden  w-56 py-4 px-8 rounded-full bg-brand-lime-red text-brand-white font-normal capitalize transition-all duration-200 ease hover:opacity-60">
+    class=" md:block hidden  w-56 py-4 px-8 rounded-full bg-brand-lime-red text-brand-white font-bold capitalize transition-all duration-200 ease hover:opacity-60">
     {{ $t('visiteCatalogue') }}
   </button>
   <button @click="gotoWandaBook"
-    class=" w-full md:hidden text-sm mx-4 py-2 my-2 rounded-full bg-brand-lime-red text-brand-white font-normal capitalize transition-all duration-200 ease hover:opacity-60">
+    class=" w-full md:hidden text-sm mx-4 py-2 my-2 mb-3 rounded-full bg-brand-lime-red text-brand-white font-bold capitalize transition-all duration-200 ease hover:opacity-60">
     {{ $t('visiteCatalogue') }}
   </button>
 </template>
