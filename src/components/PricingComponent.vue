eac3c4d6d08cb653022e9570abddc87ff8ec6543
Your Name <styvesdaudet@gmail.com>
2025-07-03 12:24:04 +0100

    update data

--- src/components/PricingComponent.vue
+++ src/components/PricingComponent.vue
@@ -122,7 +122,7 @@
 
         <div class="fixed inset-0 z-10 w-screen overflow-y-auto">
             <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
-                <div
+                <div 
                     class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-7 sm:w-full sm:max-w-lg">
                     <form @submit.prevent="createWandaUser('')">
                         <div class="bg-white px-4 sm:p-6 sm:pb-2">
